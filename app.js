de9040e909332e943f9570540c9063a1e733f2d3
AkraticCloud <christopherarevalo04@outlook.com>
2025-12-01 14:32:17 -0500

    Finished user routes, improved app for use of cookies

--- app.js
+++ app.js
@@ -10,8 +10,15 @@ const app = express()
 const PORT = process.env.SERVER_PORT || 8000
 
 
+const corsOptions = {
+   origin: process.env.ACCEPTED_ORIGIN, 
+   credentials: true,
+   methods: ['GET','POST','PATCH','PUT','DELETE'],
+   allowedHeaders: ['Content-Type', 'Authorization']
+}
+
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(express.json({limit: '10mb'})) //Arbitrary json size limit
 
